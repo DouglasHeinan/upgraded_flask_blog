663bfa5869dfe7c1a3068f06130cfcd7acac8f2d
gruvis41 <dougheinan@gmail.com>
2022-04-19 13:12:47 -0600

    updated with email capability for contact page

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+import os
+
+
+MY_EMAIL = os.environ["MY_EMAIL"]
+PASSWORD = os.environ["PASSWORD"]
 
 
 def main():
@@ -17,16 +23,32 @@ def main():
     def about_page():
         return render_template("about.html")
 
-    @app.route("/contact")
+    @app.route("/contact", methods=["POST", "GET"])
     def contact():
-        return render_template("contact.html")
+        post = False
+        if request.method == "POST":
+            post = True
+            user_name = request.form["name"]
+            # user_email = request.form["email"]
+            # user_phone = request.form["phone"]
+            message = request.form["message"]
+            with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+                connection.starttls()
+                connection.login(user=MY_EMAIL, password=PASSWORD)
+                connection.sendmail(
+                  from_addr=MY_EMAIL,
+                  to_addrs=MY_EMAIL,
+                  msg=f"Subject: {user_name} has contacted you!\n\n{message}"
+                )
+            return render_template("contact.html", post=post)
+        return render_template("contact.html", post=post)
 
     @app.route("/post_<post_id>")
     def read_post(post_id):
         entry = [entry for entry in entries if entry["id"] == post_id][0]
         return render_template("post.html", entry=entry)
 
-    app.run()
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
